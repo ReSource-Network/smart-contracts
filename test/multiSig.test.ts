2af6e3c350e2d09bc9c7c253dced38af0cc20288
Bridger Zoske <bridgerzoske@gmail.com>
2021-08-02 10:26:11 -0700

    MultiSig scripts

--- test/multiSig.test.ts
+++ test/multiSig.test.ts
@@ -12,6 +12,7 @@ describe("MultiSig Tests", function () {
   let ownerA: SignerWithAddress;
   let ownerB: SignerWithAddress;
   let ownerC: SignerWithAddress;
+  let ownerD: SignerWithAddress;
   let multiSigWallet: MultiSigWallet;
 
   before(async function () {
@@ -20,6 +21,7 @@ describe("MultiSig Tests", function () {
     ownerA = accounts[1];
     ownerB = accounts[2];
     ownerC = accounts[3];
+    ownerD = accounts[4];
   });
 
   it("Successfully deploys a multiSig wallet contract with ownerA and ownerB", async function () {
@@ -78,5 +80,51 @@ describe("MultiSig Tests", function () {
     const owners = await multiSigWallet.getOwners();
     expect(owners).to.contain(ownerA.address);
     expect(owners).to.contain(ownerC.address);
+  });
+  it("Successfully adds ownerD to the multiSig", async () => {
+    // populate replaceOwner transaction using ownerA wallet
+    const data = (await multiSigWallet.connect(ownerA).populateTransaction.addOwner(ownerD.address)).data!;
+
+    // get multiSig owner tx nonce
+    const ownerANonce = await multiSigWallet.nonces(ownerA.address);
+
+    // generate prepare submit transaction hash for signature by ownerA
+    const ownerAHashToSign = ethers.utils.arrayify(
+      await multiSigWallet.connect(ownerA).prepareSubmitTransaction(multiSigWallet.address, 0, data, ownerANonce),
+    );
+
+    // generate ownerA signature
+    const ownerASig = ethers.utils.joinSignature(await ownerA.signMessage(ownerAHashToSign));
+
+    const submissionResult = await (
+      await multiSigWallet.submitTransactionByRelay(multiSigWallet.address, 0, data, ownerASig, ownerA.address)
+    ).wait();
+
+    const transactionId = submissionResult.events?.find((e: any) => e.eventSignature == "Submission(uint256)")?.args
+      ?.transactionId;
+
+    expect(transactionId).to.equal(1);
+
+    // get nonce of ownerC
+    const ownerCNonce = await multiSigWallet.nonces(ownerC.address);
+
+    // generate prepare confirm transaction hash for signature by coSigner
+    const ownerCHashToSign = ethers.utils.arrayify(
+      await multiSigWallet.connect(ownerC).prepareConfirmTransaction(transactionId, ownerCNonce),
+    );
+
+    // generate coSigner signature
+    const ownerCSig = ethers.utils.joinSignature(await ownerC.signMessage(ownerCHashToSign));
+
+    // 3. confirmTransactionByRelay using ownerC wallet
+    await expect(multiSigWallet.confirmTransactionByRelay(transactionId, ownerCSig, ownerC.address)).to.emit(
+      multiSigWallet,
+      "Execution",
+    );
+
+    const owners = await multiSigWallet.getOwners();
+    expect(owners).to.contain(ownerA.address);
+    expect(owners).to.contain(ownerC.address);
+    expect(owners).to.contain(ownerD.address);
   });
 });
